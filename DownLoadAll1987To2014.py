7bb0a01f94c64480dbeee8c40474cfeb8c03760f
Gebreab K. Zewdie <phygbki21@gmail.com>
2018-01-06 20:27:51 -0600

    Update DownLoadAll1987To2014.py

--- DownLoadAll1987To2014.py
+++ DownLoadAll1987To2014.py
@@ -1,8 +1,14 @@
+# This simple code helps to extract weather and land surface parameters from ECMWF global data, https://www.ecmwf.int/,
+#https://www.ecmwf.int/en/forecasts/datasets/browse-reanalysis-datasets
 #!/usr/bin/env python
+
+# Import ECMWFDataServer
 from ecmwfapi import ECMWFDataServer
 server = ECMWFDataServer()
+# Set your parameters and  type od experiment,  selection eare etc, See the read me file for details!
+
 server.retrieve({
-'stream'    : "oper",
+  'stream'    : "oper",   
     'levtype'   : "sfc",
     'param'     : "50.128/57.128/58.128/142.128/143.128",
     'dataset'   : "interim",
@@ -12,7 +18,9 @@ server.retrieve({
     'date'      : "1987-01-01/to/2014-12-31",
     'type'      : "an",
     'class'     : "ei",
-    'area'      : "36.5/-96.5/35.5/-95.5",
+    'area'      : "32.8/-97/32.8/-97",
     'format'    : "netcdf",
-    'target'    : "TestDownloadmyArea1987To2014G9.nc"})
+    'target'    : "DownloadsParam1.nc"})
 
+   # area is the  an example of a place near dallas, you can also use "United Sates",  "Europe" to download over a large area.
+   # see http://apps.ecmwf.int/datasets/data/interim-full-daily/levtype=sfc/ for interactive uasage to select parameters.
